7899128597a1d3c25706469580383f826d66026b
Giridhar <giridhar.thrinadha@censanext.com>
2025-03-29 04:06:48 +0000

    "require-01"

---
+++ c.mjs
@@ -0,0 +1,3 @@
+console.log('Entered C')
+
+export default 'New'
---
+++ testrequire.js
@@ -0,0 +1,8 @@
+console.log("start");
+//require('./a')
+if(1==1){
+
+    import('./b.mjs')
+}
+import c from "./c.mjs";//Excute first in intial phase //cannot be wrapped under condition
+console.log("END");
